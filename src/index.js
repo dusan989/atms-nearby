e891067c39e2895e55ee27ecbd87cb7effb06312
Dusan Milosavljevic <mdusan989@gmail.com>
2017-07-05 12:16:05 +0200

    Ask user for geolocation.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ import showError from './error/showError';
 // eslint-disable-next-line
 require('file-loader?name=[name].[ext]!./index.html');
 
-getLocation()
-  .then(coordinates => console.log(coordinates))
-  .catch(error => showError(error));
+// Ask user to allow geolocation.
+document.getElementById('allow-geolocation').addEventListener('click', (event) => {
+  event.preventDefault();
+
+  document.getElementById('allow-geolocation-card').classList.add('hide');
+
+  getLocation()
+    .then(coordinates => console.log(coordinates))
+    .catch(error => showError(error));
+
+  return false;
+});
